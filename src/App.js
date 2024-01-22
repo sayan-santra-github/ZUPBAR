06ca5d8500f4f58ab42d884be7ddaeb9c22c8b86
sayan-santra-github <sayanmicrosoft587@gmail.com>
2024-01-22 23:29:33 +0530

    Set template engine to ejs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ require('./db/connectdb')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, '../templates/views'))
 hbs.registerPartials(path.join(__dirname, "../templates/partials"))
 app.use(express.static(path.join(__dirname, '../public')))
