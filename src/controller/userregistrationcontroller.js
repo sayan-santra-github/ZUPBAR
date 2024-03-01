69859f670959da1d2c1c72a830a732dc9221453e
sayan-santra-github <sayanmicrosoft587@gmail.com>
2024-03-01 22:44:20 +0530

    not atteded and not organized trip page setup complete

--- src/controller/userregistrationcontroller.js
+++ src/controller/userregistrationcontroller.js
@@ -383,6 +383,8 @@ const trip_history_view = async (req, res) => {
       attendTripsForTripHit.push(initialattendTripsForTripHit)
     }
 
+    console.log(attendTripsForTripHit)
+
     res.render("trip_history", { user, attendTripsForTripHit, organizedTripsForTripHit });
 
   } catch (error) {
